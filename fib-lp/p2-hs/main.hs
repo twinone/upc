5793788a062476921a93a463b77840916f49fad2
Luuk W. (Twinone) <twinonedevs@gmail.com>
2016-12-09 09:51:08 +0100

    Implement Show

--- fib-lp/p2-hs/main.hs
+++ fib-lp/p2-hs/main.hs
@@ -5,6 +5,7 @@ type Ident = String
 -- Can be any Number
 type Value = Int
 
+
 data NExpr a
   = Var     Ident
   | Const   a -- TODO parameterize
@@ -21,6 +22,10 @@ data BExpr a
   | Eq    (NExpr a) (NExpr a)
   deriving (Read, Show)
 
+data Seq a
+  = Seq [Command a]
+  deriving (Read, Show)
+
 data Command a
   = Assign  Ident (NExpr a)  -- Assign x y assigns a constant value y to x
   | Print   Ident            -- Print x prints the value of x to stdout
@@ -31,11 +36,25 @@ data Command a
   | Size    Ident Ident      -- Size x y assigns the len(x) to y
   | Cond    (BExpr a) (Seq a) (Seq a) -- Cond x y z executes if x then y else z
   | Loop    (BExpr a) (Seq a) -- Loop x y executes y while x
-  deriving (Read, Show)
+  deriving (Read)
 
-data Seq a
-  = Seq [Command a]
-  deriving (Read, Show)
+instance (Show a) => Show (Command a) where
+  show (Assign i x) = i ++ " := " ++ show x ++ "\n"
+  show (Print i)    = "PRINT " ++ i ++ "\n"
+  show (Input i)    = "INPUT " ++ i ++ "\n"
+  show (Empty i)    = "EMPTY " ++ i ++ "\n"
+  show (Pop i j)    = "POP " ++ i ++ " " ++ j ++ "\n"
+  show (Push i j)   = "PUSH " ++ i ++ " " ++ show j ++ "\n"
+  show (Size i j)   = "SIZE " ++ i ++ " " ++ j ++ "\n"
+  show (Cond c x y) = "IF " ++ show c ++ " THEN\n" ++ nest x ++ elseVal y ++ "END\n"
+    where
+      elseVal (Seq []) = ""
+      elseVal _ = "ELSE\n" ++ nest y
+  show (Loop c x)   = "WHILE " ++ show c ++ "\nDO\n" ++ nest x ++ "END\n"
+
+-- nest handles the recursive calls to show
+nest :: (Show a) => Seq a -> String
+nest (Seq x) = unlines (map (("  "++).show) x)
 
 main = do
   input <- getContents
