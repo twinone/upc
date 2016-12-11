06875a4e7f1edf20369068d22fad3292f1b2d463
Luuk W. (Twinone) <twinonedevs@gmail.com>
2016-12-11 19:23:52 +0100

    Nicer output

--- fib-lp/p2-hs/run.sh
+++ fib-lp/p2-hs/run.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
+OUT="programhs.txt"
+
 if [[ -z "$1" ]]; then
 	TEST="test/test.in"
 else
 	TEST="$1"
 fi
 
-CPLUS_INCLUDE_PATH=~/Downloads/pccts/h/ make all
 
-./parser < $TEST
 
-./parser --noast < $TEST | ./main
+CPLUS_INCLUDE_PATH=~/Downloads/pccts/h/ make clean all
+
+./parser < $TEST > $OUT
+
+echo ""
+echo "====================== PROGRAM ========================"
+echo ""
+
+cat $TEST
+
+echo ""
+echo "======================  AST    ========================"
+echo ""
+
+cat $OUT
+
+echo ""
+echo "====================== OUTPUT  ========================"
+echo ""
+
+./main < $OUT
+
+echo ""
+echo "======================================================="
+echo ""
