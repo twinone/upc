e8d2db0e718f552ba9e194f29711d7cbce5f6c17
Luuk W. (Twinone) <twinonedevs@gmail.com>
2016-12-11 20:26:37 +0100

    Refactor and comment code

--- fib-lp/p2-hs/main.hs
+++ fib-lp/p2-hs/main.hs
@@ -1,6 +1,13 @@
+-- Author: Twinone (Luuk W.)
+-- Code commented with love for a correction with love
+
+--------------------------------------------------------------------------------
+---------------------------    CONSTANTS    ------------------------------------
+--------------------------------------------------------------------------------
+
 -- errors
-typeError = "type error "
-referenceError x = "ReferenceError: " ++ x ++ " is not defined"
+typeError = "type error"
+referenceError = "undefined variable"
 emptyStackError = "empty stack"
 
 -- types
@@ -10,18 +17,14 @@ typeSym = "sym"
 
 file = "programhs.txt"
 
-
-
 -- Identifier for a variable
 type Ident = String
--- Only used in main
--- Current type used for the values of the program
--- Can be any Num
-type Value = Double
 
 --------------------------------------------------------------------------------
---------------------------         PARSING          ----------------------------
+-------------------------  DATA STRUCTURES & SHOW   ----------------------------
 --------------------------------------------------------------------------------
+
+------------------------------   NExpr    --------------------------------------
 
 data NExpr a
   = Var     Ident
@@ -38,6 +41,7 @@ instance (Show a) => Show (NExpr a) where
   show (Minus x y) = show x ++ " - " ++ show y
   show (Times x y) = show x ++ " * " ++ show y
 
+------------------------------   BExpr    --------------------------------------
 
 data BExpr a
   = AND   (BExpr a) (BExpr a)
@@ -54,9 +58,7 @@ instance (Show a) => Show (BExpr a) where
   show (Gt x y) = show x ++ " > " ++ show y
   show (Eq x y) = show x ++ " = " ++ show y
 
-
-
-
+------------------------------   Command    ------------------------------------
 
 data Command a
   = Assign  Ident (NExpr a)  -- Assign x y assigns a constant value y to x
@@ -68,7 +70,7 @@ data Command a
   | Size    Ident Ident      -- Size x y assigns the len(x) to y
   | Cond    (BExpr a) (Command a) (Command a) -- Cond x y z executes if x then y else z
   | Loop    (BExpr a) (Command a) -- Loop x y executes y while x
-  | Seq     [Command a]
+  | Seq     [Command a]       -- A sequence of commands
   deriving (Read)
 
 instance (Show a) => Show (Command a) where
@@ -79,26 +81,58 @@ instance (Show a) => Show (Command a) where
   show (Pop i j)    = "POP " ++ i ++ " " ++ j ++ "\n"
   show (Push i j)   = "PUSH " ++ i ++ " " ++ show j ++ "\n"
   show (Size i j)   = "SIZE " ++ i ++ " " ++ j ++ "\n"
-  show (Loop c x)   = "WHILE " ++ show c ++ "\nDO\n" ++ show' x ++ "END\n"
+  show (Loop c x)   = "WHILE " ++ show c ++ "\nDO\n" ++ indent x ++ "END\n"
   show (Cond c x y) = "IF " ++ show c ++ " THEN\n" ++ iv ++ ev y ++ "END\n"
+    -- only show else part when needed
     where
-      iv = show' x
+      iv = indent x
       ev (Seq []) = ""
-      ev _ = "ELSE\n" ++ show' y
-  show x = unlines $ (map (drop 2)) $ lines $ (show' x)
+      ev _ = "ELSE\n" ++ indent y
+  show x = unlines $ (map (drop 2)) $ lines $ (indent x)
 
--- show a sequence
-show' :: (Show a) => Command a -> String
-show' (Seq x) = unlines (map (rstrip. indent . show) x)
+-- show a sequence indented
+indent :: (Show a) => Command a -> String
+indent (Seq x) = unlines (map (rstrip. indent . show) x)
   where
     indent = (unlines . (map ("  "++)) . lines)
     rstrip = reverse . dropWhile (=='\n') . reverse
 
+
 --------------------------------------------------------------------------------
---------------------------        EVALUATION        ----------------------------
+--------------------------         SYMTABLE         ----------------------------
 --------------------------------------------------------------------------------
 
--- utility functions
+
+-- We're explicitly not allowed to use any imports for this part.
+-- Implementing an efficient data structure like a map is a solved problem
+-- So I'm not going to waste time reinventing the wheel as this is not a
+-- data structures subject :)
+data SymTable a = SymTable [Entry a] deriving (Show)
+data Entry a = Entry Ident (Sym a) deriving (Show)
+data Sym a = Sym a | Stack [a] deriving (Show)
+
+---------------------  BASIC OPERATIONS: set, del, get -------------------------
+set :: SymTable a -> Ident -> Sym a -> SymTable a
+set t k v = SymTable $ (Entry k v):(elems $ del t k)
+
+del :: SymTable a -> Ident -> SymTable a
+del (SymTable t) k = SymTable r
+  where r = filter ((/=k).key) t
+
+get :: SymTable a -> Ident -> Maybe (Sym a)
+get (SymTable []) _ = Nothing
+get t k
+  | k == key hd = Just (val hd)
+  | otherwise   = get (SymTable tl) k
+  where
+   hd = (head . elems) t
+   tl = (tail . elems) t
+
+
+--------------------------------------------------------------------------------
+--------------   UTILITY FUNCTIONS -- WARNING: BORING   ------------------------
+--------------------------------------------------------------------------------
+
 isLeft :: Either a b -> Bool
 isLeft (Left a) = True
 isLeft _ = False
@@ -115,13 +149,6 @@ left (Left a) = a
 
 right :: Either a b -> b
 right (Right b) = b
-
--- SymTable holds all variables
--- TODO: Make this a RB tree maybe for efficiency
--- We're not allowed to use Haskell's data structures
-data Sym a = Sym a | Stack [a] deriving (Show)
-data Entry a = Entry Ident (Sym a) deriving (Show)
-data SymTable a = SymTable [Entry a] deriving (Show)
 
 isStack :: Sym a -> Bool
 isStack (Sym a) = False
@@ -143,58 +170,47 @@ val (Entry _ v) = v
 elems :: SymTable a -> [Entry a]
 elems (SymTable t) = t
 
--- manipulation of a symtable
-set :: SymTable a -> Ident -> Sym a -> SymTable a
-set t k v = SymTable $ (Entry k v):(elems $ del t k)
-
--- sets adds all the values from the first table to the second
--- overwriting any existing values
-setAll :: SymTable a -> SymTable a -> SymTable a
-setAll (SymTable []) b = b
-setAll (SymTable (x:xs)) b = setAll (SymTable xs) (set b (key x) (val x))
-
-
-del :: SymTable a -> Ident -> SymTable a
-del (SymTable t) k = SymTable r
-  where r = filter ((/=k).key) t
-
-get :: SymTable a -> Ident -> Maybe (Sym a)
-get (SymTable []) _ = Nothing
-get t k
-  | k == key hd = Just (val hd)
-  | otherwise   = get (SymTable tl) k
+-- type of a symbol
+typeOf :: SymTable a -> Ident -> String
+typeOf t k
+  | isNothing mb  = typeNil
+  | isStack val   = typeStack
+  | otherwise     = typeSym
   where
-   hd = (head . elems) t
-   tl = (tail . elems) t
+    val = just mb
+    mb = get t k
 
-getSym :: SymTable a -> Ident -> Maybe a
-getSym (SymTable []) _ = Nothing
-getSym t k
-  | k == key hd = Just ((sym . val) hd)
-  | otherwise   = getSym (SymTable tl) k
-  where
-   hd = (head . elems) t
-   tl = (tail . elems) t
+-- bool to int
+btoi :: Num a => Bool -> a
+btoi True  = 1
+btoi False = 0
 
-getSymOrErrF :: (Ident -> Maybe a) -> Ident -> Either String a
-getSymOrErrF f x
-  | isNothing v  = Left (referenceError x)
-  | otherwise    = Right (just v)
-  where v = f x
+-- int to bool
+itob :: (Eq a, Num a) => a -> Bool
+itob 0 = False
+itob 1 = True
+
+-- boolean operation with ints, converts 1->True, 0->False and returns the
+-- result as an int
+bop :: (Eq a, Num a) => (Bool -> Bool -> Bool) -> a -> a -> a
+bop f x y = btoi ((itob x) `f` (itob y))
 
 
-getSymOrErr :: SymTable a -> Ident -> Either String a
-getSymOrErr t x = getSymOrErrF (getSym t) x
+--------------------------------------------------------------------------------
+--------------------------         EVALUABLE         ---------------------------
+--------------------------------------------------------------------------------
 
 
 class Evaluable e where
   eval      :: (Num a, Ord a) => (Ident -> Maybe a) -> (e a) -> (Either String a)
   typeCheck :: (Ident -> String) -> (e a) -> Bool
 
-
 instance Evaluable NExpr where
   eval _ (Const a) = Right a
-  eval f (Var x) = getSymOrErrF f x
+  eval f (Var x)
+    | isNothing v  = Left referenceError
+    | otherwise    = Right (just v)
+    where v = f x
   eval f (Plus x y)   = evalOp' f (+) x y
   eval f (Minus x y)  = evalOp' f (-) x y
   eval f (Times x y)  = evalOp' f (*) x y
@@ -206,6 +222,22 @@ instance Evaluable NExpr where
   typeCheck f (Times x y) = typeCheck f x && typeCheck f y
 
 
+instance Evaluable BExpr where
+  eval f (AND x y)  = evalOp' f (bop (&&)) x y
+  eval f (OR x y)   = evalOp' f (bop (||)) x y
+  -- I know, I know... ugly hack to avoid rewriting the whole function...
+  eval f (NOT x)    = evalOp' f (bop (\x y -> not x)) x x
+  eval f (Gt x y)   = evalOp' f (\a b -> btoi (a > b)) x y
+  eval f (Eq x y)   = evalOp' f (\a b -> btoi (a == b)) x y
+
+  typeCheck f (AND x y) = typeCheck f x && typeCheck f y
+  typeCheck f (OR x y)  = typeCheck f x && typeCheck f y
+  typeCheck f (NOT x )  = typeCheck f x
+  typeCheck f (Gt x y)  = typeCheck f x && typeCheck f y
+  typeCheck f (Eq x y)  = typeCheck f x && typeCheck f y
+
+
+-- utility function
 evalOp' f op x y
   | isLeft xv = xv
   | isLeft yv = yv
@@ -215,48 +247,27 @@ evalOp' f op x y
     yv = eval f y
 
 
-typeOf :: SymTable a -> Ident -> String
-typeOf t k
-  | isNothing mb  = typeNil
-  | isStack val   = typeStack
-  | otherwise     = typeSym
+-- the function for eval (Ident -> Maybe a) (we'll apply the SymTable to it)
+getSym :: SymTable a -> Ident -> Maybe a
+getSym (SymTable []) _ = Nothing
+getSym t k
+  | k == key hd = Just $ (sym . val) hd
+  | otherwise   = getSym (SymTable $ (tail . elems) t) k
   where
-    val = just mb
-    mb = get t k
-
-btoi :: Num a => Bool -> a
-btoi True  = 1
-btoi False = 0
-
-itob :: (Eq a, Num a) => a -> Bool
-itob 0 = False
-itob 1 = True
-
-bop :: (Eq a, Num a) => (Bool -> Bool -> Bool) -> a -> a -> a
-bop f x y = btoi ((itob x) `f` (itob y))
-
-instance Evaluable BExpr where
-  eval f (AND x y)  = evalOp' f (bop (&&)) x y
-  eval f (OR x y)   = evalOp' f (bop (||)) x y
-  -- I know, I know... ugly hack to avoid rewriting the whole function...
-  eval f (NOT x)    = evalOp' f (bop (\x y -> not x)) x x
-  eval f (Gt x y)   = evalOp' f (\a b -> btoi (a > b)) x y
-  eval f (Eq x y)   = evalOp' f (\a b -> btoi (a == b)) x y
-
-  -- bools always type, because at this stage we passed parsing already
-  typeCheck f (AND x y) = typeCheck f x && typeCheck f y
-  typeCheck f (OR x y)  = typeCheck f x && typeCheck f y
-  typeCheck f (NOT x )  = typeCheck f x
-  typeCheck f (Gt x y)  = typeCheck f x && typeCheck f y
-  typeCheck f (Eq x y)  = typeCheck f x && typeCheck f y
+   hd = (head . elems) t
 
 
-err :: String -> (Either String [a], SymTable a, [a])
-err x = (Left x, SymTable [], [])
 
-tc :: (Evaluable e) => SymTable a -> (e a) -> Bool
-tc t x = typeCheck (typeOf t) x
+--------------------------------------------------------------------------------
+------------------------         INTERPRETER         ---------------------------
+--------------------------------------------------------------------------------
 
+interpretProgram :: (Num a, Ord a) => [a] -> Command a -> (Either String [a])
+interpretProgram i c = res
+  where
+    (res, _, _) = interpretCommand (SymTable []) i c
+
+--------------------------------------------------------------------------------
 
 interpretCommand :: (Num a, Ord a) =>
    SymTable a -> [a] -> Command a -> (Either String [a], SymTable a, [a])
@@ -292,8 +303,8 @@ interpretCommand t i (Input k) = (Right [], set t k (Sym (head i)), tail i)
 interpretCommand t i (Empty k) = (Right [], set t k (Stack []), i)
 
 interpretCommand t i (Pop k v)
-  | isNothing mb    = err $ referenceError k
-  | not (isStack r) = err $ typeError ++ ": "++ k ++ " is not a stack"
+  | isNothing mb    = err $ referenceError
+  | not (isStack r) = err $ typeError
   | s == []         = err emptyStackError
   | otherwise       = (Right [], ot, i)
   where
@@ -307,9 +318,9 @@ interpretCommand t i (Pop k v)
 
 
 interpretCommand t i (Push k v)
-  | isNothing mb    = err $ referenceError k
-  | not (tc t v)    = err $ typeError ++ " no type push nexpr"
-  | not (isStack r) = err $ typeError ++ ": "++ k ++ " is not a stack"
+  | isNothing mb    = err $ referenceError
+  | not (tc t v)    = err $ typeError
+  | not (isStack r) = err $ typeError
   | isLeft val      = err $ left val
   | otherwise       = (Right [], ot, i)
   where
@@ -322,8 +333,8 @@ interpretCommand t i (Push k v)
 
 
 interpretCommand t i (Size k v)
-  | isNothing mb    = err $ referenceError k
-  | not (isStack r) = err $ typeError ++ ": "++ k ++ " is not a stack"
+  | isNothing mb    = err $ referenceError
+  | not (isStack r) = err $ typeError
   | s == []         = err emptyStackError
   | otherwise       = (Right [], ot, i)
   where
@@ -335,7 +346,7 @@ interpretCommand t i (Size k v)
 
 
 interpretCommand t i (Cond c x y)
-  | not (tc t c)  = err $ typeError ++ " no type if bexpr"
+  | not (tc t c)  = err $ typeError
   | isLeft val    = err $ left val
   | bval          = interpretCommand t i x
   | otherwise     = interpretCommand t i y
@@ -355,10 +366,18 @@ interpretCommand t i (Loop c x)
     intval  = right val
     val     = eval (getSym t) c
 
-interpretProgram :: (Num a, Ord a) => [a] -> Command a -> (Either String [a])
-interpretProgram i c = res
-  where
-    (res, _, _) = interpretCommand (SymTable []) i c
+-- utility functions
+err :: String -> (Either String [a], SymTable a, [a])
+err x = (Left x, SymTable [], [])
+
+tc :: (Evaluable e) => SymTable a -> (e a) -> Bool
+tc t x = typeCheck (typeOf t) x
+
+
+
+--------------------------------------------------------------------------------
+--------------------------           MAIN           ----------------------------
+--------------------------------------------------------------------------------
 
 rund :: [Double] -> String -> (Either String [Double])
 rund i c = interpretProgram i (read c :: (Command Double))
@@ -366,14 +385,14 @@ rund i c = interpretProgram i (read c :: (Command Double))
 runi :: [Int] -> String -> (Either String [Int])
 runi i c = interpretProgram i (read c :: (Command Int))
 
---------------------------------------------------------------------------------
---------------------------           MAIN           ----------------------------
---------------------------------------------------------------------------------
-
-
 main = do
-  input <- getContents
   program <- readFile file
+
+  putStrLn "Select type of variable:"
+  putStrLn "  0: Int"
+  putStrLn "  1: Double"
+  putStr " > "
+  varType <- getLine
 
   putStrLn $ show $ rund [30..100] program
   putStrLn "---"
