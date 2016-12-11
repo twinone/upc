69e0f7876041460eec50707df41faf4a5cec2839
Luuk W. (Twinone) <twinonedevs@gmail.com>
2016-12-11 12:35:03 +0100

    Make run run different test cases as a parameter

--- fib-lp/p2-hs/run.sh
+++ fib-lp/p2-hs/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+	TEST="test/test.in"
+else
+	TEST="$1"
+fi
+
 CPLUS_INCLUDE_PATH=~/Downloads/pccts/h/ make all
 
-./parser < test/test.in
+./parser < $TEST
 
-./parser --noast < test/test.in | ./main
+./parser --noast < $TEST | ./main
