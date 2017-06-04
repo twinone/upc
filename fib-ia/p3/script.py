c6180403a4e0f5c1f623972912fe60a3ae4cc870
Davigox <thedavigo95@gmail.com>
2017-06-04 13:28:56 +0200

    Final script

--- fib-ia/p3/script.py
+++ fib-ia/p3/script.py
@@ -1,18 +1,25 @@
 import random
 import subprocess
 
-plats = 50
-primers = 25
+plats = 20
+primers = 10
+incompatibles = 4
+restricciones = 4
 mincal = 300
 maxcal = 1200
+
 cal = []
 
 tipus = ["peix", "sopa", "carn", "pasta", "arroz", "fruta", "vegetales", "pure", "pan"]
+dies = ["lu", "ma", "mi", "ju", "vi"]
 
 f = open('test.pddl', 'w')
 f.write("(define (problem menu1)\n")
 f.write("(:domain menu)\n")
-f.write("(:objects lu ma mi ju vi - dia ")
+f.write("(:objects ")
+for item in dies:
+	f.write(item+" ")
+f.write("- dia ")
 for item in tipus:
 	f.write(item+" ")
 f.write("- tipus ")
@@ -25,6 +32,11 @@ for i in range(plats-primers):
   cal.append(str(random.randint(mincal, maxcal)))
 f.write("- segon)\n")
 f.write("(:init ")
+f.write("(diainici lu) (seguent lu ma) (seguent ma mi) (seguent mi ju) (seguent ju vi) ")
+for i in range(incompatibles):
+	f.write("(incompatible p"+str(random.randint(0, primers-1))+" p"+str(random.randint(primers, plats-1))+") ")
+for i in range(restricciones):
+	f.write("(restriccio "+random.choice(dies)+" p"+str(random.randint(0, plats-1))+") ")
 for i in range(plats):
 	if i < primers:
 		f.write("(primer p"+str(i)+" "+random.choice(tipus)+") ")
@@ -35,7 +47,7 @@ f.write("(= (total-cal) 0) ")
 for i in range(plats):
 	f.write("(= (calories p"+str(i)+") "+cal.pop()+") ")
 f.write("\n")
-f.write("(diainici lu) (seguent lu ma) (seguent ma mi) (seguent mi ju) (seguent ju vi)")
+
 f.write(")\n")
 f.write("(:goal (and (diac lu) (diac ma) (diac mi) (diac ju) (diac vi)))\n")
 f.write("(:metric minimize (total-cal))\n")
